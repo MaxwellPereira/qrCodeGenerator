22da0af2d946a261d595a8d4ed1534bf78d84a53
Maxwell Silva <maxwell.silva@agristar.pdc>
2021-06-17 13:53:19 -0300

    First commit

---
+++ createQRCodePNGFiles.py
@@ -0,0 +1,14 @@
+import re
+import pyqrcode
+import png
+
+arq = open('.\\text\\arq.txt', 'r')
+dirImg = '.\\imgs\\'
+list = arq.readlines()
+arq.close()
+for i, xLin in enumerate(list):
+    name = re.sub('\n','',list[i][list[i].rfind('/')+1:len(list[i])])
+    link = re.sub('\n','',list[i])
+    url = pyqrcode.create(link)
+    #pode-se utilizar svg
+    url.png(dirImg + name + '.png', scale=30)
